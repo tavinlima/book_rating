d6351c1551b074a37acc7c11fca9cc96184a54ec
Gustavo Lima <glima.0913@gmail.com>
2025-09-06 21:24:37 -0300

    test: Criado shouldNotTransform para quando não existir uma DTO equivalente para a classe

---
+++ src/main/java/br/ifsp/com/Publisher.java
@@ -0,0 +1,11 @@
+package br.ifsp.com;
+
+public class Publisher {
+    private String name;
+    private String cnpj;
+
+    public Publisher(String name, String cnpj) {
+        this.name = name;
+        this.cnpj = cnpj;
+    }
+}
--- src/test/java/br/ifsp/com/refl/TransformatorTest.java
+++ src/test/java/br/ifsp/com/refl/TransformatorTest.java
@@ -3,6 +3,7 @@ package br.ifsp.com.refl;
 import br.ifsp.com.Book;
 import br.ifsp.com.BookDTO;
 
+import br.ifsp.com.Publisher;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
@@ -10,6 +11,7 @@ import java.lang.reflect.InvocationTargetException;
 
 public class TransformatorTest {
     Book book = new Book(1,"Morro dos Ventos Uivantes","Emily Brontë","Editora Globo", 50.99f,100);
+    Publisher publisher = new Publisher("Editora Seguinte", "45192875000170");
 
     @Test
     public void shouldTransform() throws ClassNotFoundException, InvocationTargetException, NoSuchMethodException, InstantiationException, IllegalAccessException {
@@ -21,6 +23,14 @@ public class TransformatorTest {
         Assertions.assertEquals(book.getAuthor(), bookDTO.getAuthor());
         Assertions.assertEquals(book.getPublisher(), bookDTO.getPublisher());
         Assertions.assertEquals(book.getPrice(), bookDTO.getPrice());
+    }
+
+    @Test
+    public void shouldNotTransform() {
+        Assertions.assertThrows(ClassNotFoundException.class, () -> {
+            Transformator transformator = new Transformator();
+            transformator.transform(publisher);
+        });
     }
 
 }
